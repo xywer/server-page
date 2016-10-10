5f0deb418b1b69046f3d3309b5d6f62833926b4c
migu3ln <kalexmiguelalba@gmail.com>
2016-10-10 15:56:36 -0500

    ADAD
    AD

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-var port_listen = 6969;
-var port_mysql = 3306;
-var puerto_io = 3000;
-//----------ASIGNAR LA CONFIGURACION DE LA BDD(NOMBRE Y PUERTO Y PASS)---------
-var params_bdd = {user: "pekesc5_meetclic", password: "meetclic@", host: "creativeweb.com.ec", port: port_mysql, database: "pekesc5_xywer"};
-//var params_bdd = {user: "pekesc5_meetclic", password: "meetclic@", host: "creativeweb.com.ec", port: port_mysql, database: "pekesc5_lady"};
-//*********************MYSQL*****************
-//-------------------INIT MODULOS A UTILIZAR-------------
-//MODULO DE NODE JS PARA LA CONECCION DE LA BDD DE MYSQL
-var mysql = require('mysql');//para la comunicacion con la bdd 
-var express = require('express')//EL ESL L ENCARGADO DE LA COMUNCION DE URLS 
-        , cors = require('cors')//EL NOS FACILITA LA COMUNICACION A ESAS URLS  ACCESO A ESA URL
-        , app = express();
-app.use(cors());
-var io = require('socket.io').listen(puerto_io);//REALIZA UN PUENTE ENTRE TU APP-SISTEMA DE GESTION ---COMUNICACION ENTRE LOS DOS HACIA TU SERVIDOR
-//-------------------END MODULOS A UTILIZAR-------------
 
-//--------CONECCCION DE LA BDD--------
-var connection = mysql.createConnection(params_bdd);
 
-//var express = require('express');
-//var app = express();
+var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
