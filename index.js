5bd905df0f43366d213dc2c4a0bd483404700136
migu3ln <kalexmiguelalba@gmail.com>
2016-10-11 08:27:22 -0500

    adad
    adad

--- index.js
+++ index.js
@@ -28,19 +28,19 @@ var mysql = require('mysql');//para la comunicacion con la bdd
 var express = require('express');
 var app = express();
 //--------init CONECCCION DE LA BDD--------
-//var connection = mysql.createConnection(params_bdd);
+var connection = mysql.createConnection(params_bdd);
 //--------end CONECCCION DE LA BDD--------
 //---END PERSONA--
-//connection.connect(function (err) {
-//    if (err) {
-//        console.log('---------------------------------------Error connecting to Db------------------',err);
-//        return;
-//    } else {
-//
-//        console.log('----------------------Connection established--------------------------');
-//    }
-//
-//});
+connection.connect(function (err) {
+    if (err) {
+        console.log('---------------------------------------Error connecting to Db------------------',err);
+        return;
+    } else {
+
+        console.log('----------------------Connection established--------------------------');
+    }
+
+});
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
